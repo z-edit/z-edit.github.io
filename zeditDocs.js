69638ecc69b40304b269c442ffdf366938336281
Colin <why101@msn.com>
2018-05-15 02:19:28 -0700

    now should be able to run in both development and production

--- zeditDocs.js
+++ zeditDocs.js
@@ -585,13 +585,6 @@ ngapp.service('protocolService', function($document) {
 ngapp.service('resourceService', function($q, $http) {
     var service = this;
 
-    var getBasePath = function() {
-        var n = location.pathname.indexOf('/docs');
-        return '/' + location.pathname.slice(0, n);
-    };
-
-    var basePath = getBasePath();
-
     var retrieveResource = function(action, path, name) {
         $http.get(path).then(function(r) {
             service[name] = r.data;
@@ -605,8 +598,7 @@ ngapp.service('resourceService', function($q, $http) {
 
     this.get = function(path, name) {
         var action = $q.defer();
-        if (path[0] === '/') path = path.slice(1);
-        path = basePath + path;
+        if (path[0] !== '/') path = '/' + path;
         if (!name) name = path;
         service.hasOwnProperty(name) ?
             action.resolve(service[name]) :
